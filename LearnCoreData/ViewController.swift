d3a985954c26a03932df33e33c04203736675e8b
duc <ducnnd00568@fpt.edu.vn>
2020-06-13 14:19:06 +0700

    Initial Commit

---
+++ LearnCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearnCoreData
+//
+//  Created by Ngoduc on 6/13/20.
+//  Copyright © 2020 com.techmaster.D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
